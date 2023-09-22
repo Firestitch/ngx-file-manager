7d2222e27a4150c9ce165545a8e9695391367395
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 12:52:34 -0400

    npm cleanup

--- playground/app/components/kitchen-sink/kitchen-sink.component.ts
+++ playground/app/components/kitchen-sink/kitchen-sink.component.ts
@@ -1,11 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
-import { KitchenSinkConfigureComponent } from '../kitchen-sink-configure';
+import { FsApi } from '@firestitch/api';
 import { FsExampleComponent } from '@firestitch/example';
 import { FsMessage } from '@firestitch/message';
-import { FsApi } from '@firestitch/api';
-import { FsFileManagerComponent } from 'src/app/components/file-manager';
-import { map } from 'rxjs/operators';
 import { FsFileManagerConfig } from '@firestitch/package';
+import { map } from 'rxjs/operators';
+import { FsFileManagerComponent } from 'src/app/components/file-manager';
 
 @Component({
   selector: 'kitchen-sink',
@@ -22,9 +21,9 @@ export class KitchenSinkComponent {
     fetch: (path) => {
       const query = { path: path };
       return this.api.get(this._url, query)
-      .pipe(
-        map(response => response.data.storage_objects)
-      );
+        .pipe(
+          map(response => response.data.storage_objects)
+        );
     },
     deleteFile: (path) => {
       const query = { path: path };
@@ -41,23 +40,24 @@ export class KitchenSinkComponent {
     download: (path) => {
       const query = { path: path };
       return this.api.get(this._url + '/download', query)
-      .pipe(
-        map(response => response.data.url)
-      );
+        .pipe(
+          map(response => response.data.url)
+        );
     },
     upload: (path, file) => {
       const query = { path: path, file: file, filename: file.name };
       return this.api.post(this._url + '/upload', query)
-      .pipe(
-        map(response => response.data.storage_object)
-      );
+        .pipe(
+          map(response => response.data.storage_object)
+        );
     }
   };
 
-  constructor(private exampleComponent: FsExampleComponent,
-              private message: FsMessage,
-              private api: FsApi) {
-    exampleComponent.setConfigureComponent(KitchenSinkConfigureComponent, { config: this.config });
+  constructor(
+    private exampleComponent: FsExampleComponent,
+    private message: FsMessage,
+    private api: FsApi
+  ) {
 
   }
 }
