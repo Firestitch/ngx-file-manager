2a07273d078695a14c53745a980904ae69012882
Raymond Gigliotti <ray@firestitch.com>
2023-09-22 11:15:03 -0400

    npm cleanup

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,34 +1,33 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
+import { FsApiModule } from '@firestitch/api';
 import { FsExampleModule } from '@firestitch/example';
+import { FsLabelModule } from '@firestitch/label';
 import { FsMessageModule } from '@firestitch/message';
 import { FsFileManagerModule } from '@firestitch/package';
-import { FsApiModule } from '@firestitch/api';
-import { FsLabelModule } from '@firestitch/label';
-import { ToastrModule } from 'ngx-toastr';
 
-import { AppMaterialModule } from './material.module';
-import {
-  KitchenSinkComponent,
-  ExamplesComponent
-} from './components';
-import { AppComponent } from './app.component';
-import { KitchenSinkConfigureComponent } from './components/kitchen-sink-configure';
+import { FsFileModule } from '@firestitch/file';
 import { FsListModule } from '@firestitch/list';
 import { FsScrollModule } from '@firestitch/scroll';
 import { FsSelectionModule } from '@firestitch/selection';
-import { FsFileModule } from '@firestitch/file';
+import { AppComponent } from './app.component';
+import {
+  ExamplesComponent,
+  KitchenSinkComponent
+} from './components';
+import { KitchenSinkConfigureComponent } from './components/kitchen-sink-configure';
+import { AppMaterialModule } from './material.module';
 
 const routes: Routes = [
   { path: '', component: ExamplesComponent },
 ];
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     FsFileManagerModule.forRoot(),
@@ -39,7 +38,6 @@ const routes: Routes = [
     FsSelectionModule,
     FsExampleModule.forRoot(),
     FsMessageModule.forRoot(),
-    ToastrModule.forRoot({ preventDuplicates: true }),
     RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
     FsApiModule.forRoot(),
     FsListModule.forRoot(),
